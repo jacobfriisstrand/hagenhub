dca9a6617eb00be819b29e0a3a89f8543b93d6d2
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2025-03-17 15:50:26 +0100

    Header component created

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,13 @@
+import Logo from './Logo';
+import SearchBar from './SearchBar';
+import Menu from './Menu';
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Logo />
+      <SearchBar />
+      <Menu />
+    </header>
+  );
 }
