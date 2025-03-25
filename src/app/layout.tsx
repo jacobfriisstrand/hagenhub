642ce6613ab492dea4b2fcedf71b701b5dc6c86e
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-03-25 11:06:50 +0100

    Rename Header component to HeaderBar for consistency in naming across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
 
-import Header from "@/components/header/header";
+import HeaderBar from "@/components/header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
+        <HeaderBar />
         <main>{children}</main>
       </body>
     </html>
