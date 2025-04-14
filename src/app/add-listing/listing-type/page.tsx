6229c07a2c302c86547a630f8970bd03fad9280d
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-14 16:53:31 +0200

    Chore/add prisma seed new (#91)
    * Add listing feature: create pages for adding listings, guests, images, listing type, location, and rooms, along with the AddListingForm component for user input.
    
    * added todo
    
    * fix eslint errors
    
    * chore: update types and prisma seed script in package files and install typescript ts-node @types/node
    
    * chore: update prisma studio command to generate client before starting and set restart policy
    
    * chore: add prisma seed file

---
+++ src/app/add-listing/listing-type/page.tsx
@@ -0,0 +1,7 @@
+export default function ListingTypePage() {
+  return (
+    <div>
+      <h1>Listing Type</h1>
+    </div>
+  );
+}
---
+++ src/app/add-listing/page.tsx
@@ -0,0 +1,9 @@
+import { AddListingForm } from "@/app/features/listings/components/add-listing-form";
+
+export default function AddListingPage() {
+  return (
+    <section>
+      <AddListingForm />
+    </section>
+  );
+}
