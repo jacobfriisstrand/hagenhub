e5cadda358283531fd5bd89d1a4c95436d808f55
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-23 15:00:18 +0200

    feat: extend add-listing-schema to include guests, street name, and street number

--- src/app/features/listings/schemas/add-listing-schema.ts
+++ src/app/features/listings/schemas/add-listing-schema.ts
@@ -9,7 +9,10 @@ export const AddListingSchema = ListingSchema.pick({
   listing_night_price: true,
   listing_area_fk: true,
   listing_bedrooms: true,
+  listing_guests: true,
   listing_zip_code: true,
+  listing_street_name: true,
+  listing_street_number: true,
 }).extend({
   listing_images: z.array(
     ListingImageSchema.pick({
