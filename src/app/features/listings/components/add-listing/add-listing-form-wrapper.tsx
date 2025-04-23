5cda5dd7edffe87448cafeb56d54f4994dc3ff64
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-23 15:01:57 +0200

    fix: improve layout and responsiveness of add-listing-form-wrapper component

--- src/app/features/listings/components/add-listing/add-listing-form-wrapper.tsx
+++ src/app/features/listings/components/add-listing/add-listing-form-wrapper.tsx
@@ -12,14 +12,14 @@ type FormWrapperProps = {
 
 export default function FormWrapper({ children, title, icon }: FormWrapperProps) {
   return (
-    <div className="mx-auto flex items-center justify-center h-full">
-      <section className="grid grid-cols-1 lg:grid-cols-[20rem_1fr_40rem] lg:grid-rows-[20rem] gap-10">
-        <div className="space-x-2 flex items-center h-fit">
+    <div className="flex items-center justify-center h-full">
+      <section className="grid grid-cols-1 lg:grid-cols-[20rem_1px_40rem] justify-center min-h-full lg:min-h-1/2 gap-5 lg:gap-10 w-full">
+        <div className="space-x-2 lg:space-x-5 flex items-center h-fit">
           <DynamicIcon className="text-gray-300 xl:size-10" name={icon} />
           <h1 className="text-2xl xl:text-4xl font-bold">{title}</h1>
         </div>
         <Separator orientation="vertical" className="hidden lg:block" />
-        <Card className="p-8">
+        <Card className="p-8 lg:aspect-auto min-h-fit">
           {children}
         </Card>
       </section>
