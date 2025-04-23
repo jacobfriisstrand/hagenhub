36b8e8ba27278d256b713d765418fe6538eb805f
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-23 10:40:08 +0200

    feat: wrap add-listing-form in a centered div for improved layout

--- src/app/features/listings/components/add-listing/add-listing-form-wrapper.tsx
+++ src/app/features/listings/components/add-listing/add-listing-form-wrapper.tsx
@@ -12,15 +12,17 @@ type FormWrapperProps = {
 
 export default function FormWrapper({ children, title, icon }: FormWrapperProps) {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-[20rem_1fr_40rem] lg:grid-rows-[20rem] gap-10">
-      <div className="space-x-2 flex items-center h-fit">
-        <DynamicIcon className="text-gray-300 xl:size-10" name={icon} />
-        <h1 className="text-2xl xl:text-4xl font-bold">{title}</h1>
-      </div>
-      <Separator orientation="vertical" className="hidden lg:block" />
-      <Card className="p-8">
-        {children}
-      </Card>
-    </section>
+    <div className="mx-auto flex items-center justify-center h-full">
+      <section className="grid grid-cols-1 lg:grid-cols-[20rem_1fr_40rem] lg:grid-rows-[20rem] gap-10">
+        <div className="space-x-2 flex items-center h-fit">
+          <DynamicIcon className="text-gray-300 xl:size-10" name={icon} />
+          <h1 className="text-2xl xl:text-4xl font-bold">{title}</h1>
+        </div>
+        <Separator orientation="vertical" className="hidden lg:block" />
+        <Card className="p-8">
+          {children}
+        </Card>
+      </section>
+    </div>
   );
 }
