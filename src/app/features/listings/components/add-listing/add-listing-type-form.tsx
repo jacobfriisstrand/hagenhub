1d32b6358d4b0fb68cab79cad16cc55954b7d5ba
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-23 15:03:09 +0200

    feat: enhance add-listing-type-form layout with form-group component

--- src/app/features/listings/components/add-listing/add-listing-type-form.tsx
+++ src/app/features/listings/components/add-listing/add-listing-type-form.tsx
@@ -12,7 +12,8 @@ import FormWrapper from "@/app/features/listings/components/add-listing/add-list
 import AddListingTypeRadioButton from "@/app/features/listings/components/add-listing/add-listing-type-radio-button";
 import { AddListingSchema } from "@/app/features/listings/schemas/add-listing-schema";
 import { Button } from "@/components/ui/button/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form/form";
+import { FormGroup } from "@/components/ui/form/form-group";
 import { RadioGroup } from "@/components/ui/radio-group";
 
 const addListingTypeSchema = AddListingSchema.pick({
@@ -40,43 +41,42 @@ export default function AddListingTypeForm({ listingTypes }: { listingTypes: Lis
   return (
     <FormWrapper title="Listing type" icon="house">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-between h-full gap-10">
           <FormField
             control={form.control}
             name="listing_type_fk"
             render={({ field }) => (
-              <FormItem className="space-y-3">
-                <div className="space-y-2">
+              <FormGroup title="Listing Type" description="Choose the type of accommodation you want to add">
+                <FormItem className="gap-10">
                   <FormLabel className="text-lg font-medium sr-only">Listing type</FormLabel>
-                  <FormDescription>Choose the type of accommodation you want to add</FormDescription>
-                </div>
-                <FormControl>
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    value={field.value}
-                    className="grid lg:grid-cols-3 gap-4"
-                    aria-invalid={!!form.formState.errors.listing_type_fk}
-                    aria-errormessage={form.formState.errors.listing_type_fk?.message}
-                  >
-                    {listingTypes.map((type: ListingType) => (
-                      <FormItem
-                        key={type.listing_type_pk}
-                        className="flex items-center space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <AddListingTypeRadioButton
-                            isChecked={field.value === type.listing_type_pk}
-                            value={type.listing_type_pk}
-                            icon={type.listing_type_icon}
-                            formLabel={type.listing_type_name}
-                          />
-                        </FormControl>
-                      </FormItem>
-                    ))}
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      className="grid lg:grid-cols-3 gap-4"
+                      aria-invalid={!!form.formState.errors.listing_type_fk}
+                      aria-errormessage={form.formState.errors.listing_type_fk?.message}
+                    >
+                      {listingTypes.map((type: ListingType) => (
+                        <FormItem
+                          key={type.listing_type_pk}
+                          className="flex items-center space-x-3 space-y-0"
+                        >
+                          <FormControl>
+                            <AddListingTypeRadioButton
+                              isChecked={field.value === type.listing_type_pk}
+                              value={type.listing_type_pk}
+                              icon={type.listing_type_icon}
+                              formLabel={type.listing_type_name}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </FormGroup>
             )}
           />
           <Button
