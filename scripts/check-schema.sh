71fba5b0b553169dd1a7b3289258928a1f230a3c
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-24 14:50:34 +0200

    chore: update error message in schema check script to reflect new migration command

--- scripts/check-schema.sh
+++ scripts/check-schema.sh
@@ -4,7 +4,7 @@ if [ -n "$(git diff --cached --name-only prisma/schema.prisma)" ]; then
   if [ -z "$(git diff --cached --name-only prisma/migrations/*.sql)" ]; then
     echo
     echo "Error: Schema changes detected but no migration file found."
-    echo "Please run \"npx prisma migrate dev\" to create a migration"
+    echo "Please run \"npx prisma migrate:branch\" to create a migration with the current branch name"
     echo
     exit 1
   fi
