b8df1986506038bafe5f6c36f191b12a84d2bf74
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-04-28 14:56:09 +0200

    fix: adjust min height of add listing form section for better layout

--- src/app/features/listings/components/add-listing/add-listing-form-wrapper.tsx
+++ src/app/features/listings/components/add-listing/add-listing-form-wrapper.tsx
@@ -13,7 +13,7 @@ type FormWrapperProps = {
 export default function FormWrapper({ children, title, icon }: FormWrapperProps) {
   return (
     <div className="flex items-center justify-center h-full">
-      <section className="grid grid-cols-1 lg:grid-cols-[20rem_1px_40rem] justify-center min-h-full lg:min-h-1/2 gap-5 lg:gap-10 w-full">
+      <section className="grid grid-cols-1 lg:grid-cols-[20rem_1px_40rem] justify-center min-h-full lg:min-h-[60%] gap-5 lg:gap-10 w-full">
         <div className="space-x-2 lg:space-x-5 flex items-center h-fit">
           <DynamicIcon className="text-gray-300 xl:size-10" name={icon} />
           <h1 className="text-2xl xl:text-4xl font-bold">{title}</h1>
