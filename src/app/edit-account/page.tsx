98a9cc75933d2eec1b3d937349c02a48459553bd
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2025-05-04 11:49:40 +0200

    feat: add @radix-ui/react-accordion dependency and update user model in Prisma schema
    - Added `@radix-ui/react-accordion` to dependencies in `package.json` and `package-lock.json`.
    - Enhanced `User` model in Prisma schema with new fields: `user_zip_code`, `user_street_name`, and `user_street_number`.
    - Updated validation regex for `user_first_name`, `user_last_name`, `user_phone_number`, and added new validation for `user_zip_code` and `user_street_number`.
    - Modified generated Zod schema to reflect changes in the User model.

---
+++ src/app/edit-account/page.tsx
@@ -0,0 +1,50 @@
+import EditEmailForm from "@/app/features/users/components/edit-account/edit-email-form";
+import FirstLastNameForm from "@/app/features/users/components/edit-account/edit-first-last-name-form";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import { getCurrentUser } from "../(auth)/current-user";
+import EditPhoneForm from "../features/users/components/edit-account/edit-phone";
+import EditUserAddress from "../features/users/components/edit-account/edit-user-address";
+
+const fullUser = await getCurrentUser({
+  withFullUser: true,
+  redirectIfNotFound: true,
+});
+
+export default function EditAccount() {
+  return (
+    <>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="first-last-name">
+          <AccordionTrigger>Dit fulde navn</AccordionTrigger>
+          <AccordionContent>
+            <FirstLastNameForm firstName={fullUser.user_first_name} lastName={fullUser.user_last_name} user_pk={fullUser.user_pk} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="email">
+          <AccordionTrigger>E-mailadresse</AccordionTrigger>
+          <AccordionContent>
+            <EditEmailForm user_email={fullUser.user_email} user_pk={fullUser.user_pk} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="phone">
+          <AccordionTrigger>Telefonnummer</AccordionTrigger>
+          <AccordionContent>
+            <EditPhoneForm user_phone={fullUser.user_phone_number} user_pk={fullUser.user_pk} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="address">
+          <AccordionTrigger>Adresse</AccordionTrigger>
+          <AccordionContent>
+            <EditUserAddress user_zip_code={fullUser.user_zip_code} user_street_name={fullUser.user_street_name} user_street_number={fullUser.user_street_number} user_pk={fullUser.user_pk} />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
+}
