ad59b6ba47117a245b34a108b5f84a23ec25acd4
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2025-06-02 14:54:53 +0200

    refactor: update searchParams type in SearchPage component to use Promise
    - Changed the type of searchParams from a synchronous object to a Promise, enhancing the handling of asynchronous data fetching in the SearchPage component.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,7 @@ import ListingList from "@/app/features/listings/components/listing-list";
 import getListingBySearch from "../features/listings/actions/get-listing-by-search";
 
 type SearchPageProps = {
-  searchParams: { search: string };
+  searchParams: Promise<{ search: string }>;
 };
 
 export default async function SearchPage({ searchParams }: SearchPageProps) {
