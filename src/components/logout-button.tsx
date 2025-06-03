0fa79e65e03350a3e36d26ca8fefd85213cd7a3d
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2025-06-03 14:53:13 +0200

    feat: enhance homepage and footer with new components and improved layout
    - Introduced a HeroSection component for a more engaging homepage experience, featuring a search bar and quick filters for listing types.
    - Updated the Footer component to include dynamic content such as top listing areas and user account links, enhancing user navigation.
    - Refactored the Header component to improve styling and responsiveness.
    - Added a LinkWithIcon component for consistent icon usage across links.
    - Updated the page to remove unnecessary user information display and streamline the layout.

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { logout } from "@/app/(auth)/actions";
 import { Button } from "@/components/ui/button/button";
 
-import { DynamicIcon } from "./dynamic-icon";
+import { DynamicIcon } from "@/components/dynamic-icon";
 
 export default function LogoutButton() {
   const router = useRouter();
@@ -18,6 +18,7 @@ export default function LogoutButton() {
       className="hover:bg-transparent hover:text-red-500 hover:cursor-pointer p-0 m-0 h-fit py-1.5 px-2 flex justify-start max-w-full"
       variant="ghost"
     >
+      <DynamicIcon name="log-out" className="w-4 h-4" />
       <DynamicIcon name="log-out" className="w-4 h-4" />
       Logout
     </Button>
